9c240d17f0510a2a6590c6d4108e4d39a8169648
Victor Feyens <vfe@odoo.com>
2020-11-09 16:37:17 +0100

    [FIX] bring back as much as possible from the config...

--- conf.py
+++ conf.py
@@ -260,6 +260,60 @@ latex_documents = [
 # the title page.
 latex_logo = '_static/banners/odoo_logo.png'
 
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+latex_show_urls = "True"
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'understandingaccountingforentrepreneurs', u'Understanding Accounting For Entrepreneurs Documentation',
+     [u'fp, xmo'], 1)
+]
+
+# If true, show URL addresses after external links.
+#man_show_urls = False
+
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+  ('index', 'UnderstandingAccountingForEntrepreneurs', u'Understanding Accounting For Entrepreneurs Documentation',
+   u'fp, xmo', 'UnderstandingAccountingForEntrepreneurs', 'One line description of project.',
+   'Miscellaneous'),
+]
+
+# Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+
+# If false, no module index is generated.
+#texinfo_domain_indices = True
+
+# How to display URL addresses: 'footnote', 'no', or 'inline'.
+#texinfo_show_urls = 'footnote'
+
+# If true, do not generate a @detailmenu in the "Top" node's menu.
+#texinfo_no_detailmenu = False
+
+odoo_cover_external = {}
+
 # default must be set otherwise ifconfig blows up
 todo_include_todos = False
 
